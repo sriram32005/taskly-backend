17965d712b94a1048e8bcf698a6cfeac6a27d575
sriram32005 <sriram32005@gmail.com>
2026-04-07 10:40:32 +0530

    feat: add user models and migrations

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"os"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/sriram32005/taskly-backend/config"
+	"github.com/sriram32005/taskly-backend/models"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file: " + err.Error())
+	}
+
+	if err := config.DB.AutoMigrate(&models.User{}); err != nil {
+		log.Panic("Error during auto migration: ", err)
 	}
 
 	r := gin.Default()
