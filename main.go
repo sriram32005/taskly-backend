48b04f4d9988745d369331a01050078ee71d0f11
sriram32005 <sriram32005@gmail.com>
2026-04-10 10:53:37 +0530

    chore: update cors

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://172.17.0.4:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
